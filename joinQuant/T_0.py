f2d4be738b637cc0b64146ddd941d662802e6408
LoveYakamoz <yangpei3720@gmail.com>
2017-06-18 17:38:58 +0800

    Feature: [T_0]: 在沪深300中，满足最近5个交易日的波动率均大于2%, 且股价在10.00元以上的，数量为：30 （ 初始资金1亿， 平均购买标的股票各100万元）

--- joinQuant/T_0.py
+++ joinQuant/T_0.py
@@ -1,17 +1,43 @@
 from jqdata import *
 import numpy as np
+import pandas as pd
 import talib
 
 
 def initialize(context):
     log.info("---> initialize @ %s" % (str(context.current_dt))) 
-
+    g.basestock_df = pd.DataFrame(columns=("stock", "close", "min_vol", "max_vol"))
+    g.count = 0
+    g.firstrun = True
+    g.first_value = 1000000
+    g.total_value = 100000000
+    g.position_count = 30
     # 获得沪深300的股票列表, 5天波动率大于2%，单价大于10.00元, 每标的买入100万元
     stock_list = get_index_stocks('399300.XSHE')
     for stock in stock_list:
+        df = get_price(stock, count = 6, end_date = str(context.current_dt), frequency = 'daily', fields = ['high','low', 'close'])
+        for i in range(5):
+            df.ix[i+1, 'Volatility'] = (df.ix[i, 'high'] - df.ix[i, 'low']) / df.ix[i+1, 'close']
+        
+        #选择单价大于10.0元
+        if df.ix[5, 'close'] > 10.00:
+            vol_df = df.ix[1:6,['Volatility']].sort(["Volatility"], ascending=True)
+            min_vol = vol_df.iat[0, 0]
+            max_vol = vol_df.iat[4, 0]
+            #选择最近5天的波动率最小值大于0.02
+            if min_vol > 0.02:
+                g.basestock_df.loc[g.count] = [stock, df.ix[5, 'close'], min_vol, max_vol]
+                g.count += 1
+            else:
+                pass
+        else:
+            pass
+        
+    print g.basestock_df, g.count
 
+    
     # 设置基准
-    set_benchmar('000300.XSHG')
+    set_benchmark('000300.XSHG')
     set_option('use_real_price', True)
     pass
 
@@ -24,9 +50,14 @@ def before_trading_start(context):
 
 
 def handle_data(context, data):
+    # 0. 购买30支股票
+    if g.firstrun is True:
+        for i in range(g.position_count):
+            print "Buy[%d]---> stock: %s, value: %d" % (i + 1, g.basestock_df.ix[i, 0], g.first_value)
+            #order_value(g.basestock_df.ix[i, 0], 1000000)
+        g.firstrun = False
+    
     # 1. 循环股票列表，看当前价格是否有买入或卖出信号
-
-    pass
 
 
 def after_trading_data(context):
