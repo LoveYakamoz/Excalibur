7d25ad1d004df2c248f44c904841b2c182f7eee1
yangpei3720 <yangpei3720@gmail.com>
2017-08-14 21:30:05 +0800

    Bugfix: RSI计算需要3倍以上数据

--- joinQuant/Pull_Wool/Pull_Wool.py
+++ joinQuant/Pull_Wool/Pull_Wool.py
@@ -444,7 +444,7 @@ def sort_by_market_cap(context, stock_list):
 
 def get_rsi(context, data, stock):
     close = []
-    hData = attribute_history(stock, g.rsi_day_count*2, unit='1d'
+    hData = attribute_history(stock, g.rsi_day_count*3, unit='1d'
                     , fields=('close')
                     , skip_paused=True
                     , df=False)
